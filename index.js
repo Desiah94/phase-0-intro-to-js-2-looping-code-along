f6d4375eeccc58a216e4b0d15216f4ac97278b78
Desiah Barnett <desiahbarnett@gmail.com>
2023-12-29 11:27:05 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let thankYouMessages = [];
+  
+    names.forEach(function(name) {
+        let message = `Thank you, ${name}, for the wonderful surprise gift!`;
+      thankYouMessages.push(message);
+    });
+  
+    return thankYouMessages;
+  }
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+}
